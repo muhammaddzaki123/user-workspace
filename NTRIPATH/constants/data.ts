f6697b349e86197451a6c8546ed533291459b958
F1D022143_M.DZAKI AL-Q. <115340796+muhammaddzaki123@users.noreply.github.com>
2025-05-27 02:38:22 +0800

    Add NTRIPATH/constants/data.ts 1748284701770

---
+++ NTRIPATH/constants/data.ts
@@ -0,0 +1,73 @@
+import images from "./images";
+import icons from "./icons";
+export const Artikel = [
+ {
+    title: "artikel1 ovi",
+    location: "Lombok",
+    price: "Rp.1000.000",
+    rating: 4.8,
+    category: "artikel1",
+    image: images.artikel1,
+  },
+  {
+    title: "artikel1 Abdi",
+    location: "Location 2",
+    price: "Rp.1000.000",
+    rating: 3,
+    category: "artikel1",
+    image: images.artikel1,
+  },
+  {
+    title: "artikel1 ario",
+    location: "lotim",
+    price: "Rp.1000.000",
+    rating: 2,
+    category: "artikel1",
+    image: images.artikel1,
+  },
+  {
+    title: "Card 4",
+    location: "narmada",
+    price: "Rp.1000.000",
+    rating: 5,
+    category: "artikel1",
+    image: images.artikel1,
+  },
+];
+
+// export const settings = [
+//   {
+//     title: "My Bookings",
+//     icon: icons.calendar,
+//   },
+//   {
+//     title: "Payments",
+//     icon: icons.wallet,
+//   },
+//   {
+//     title: "Profile",
+//     icon: icons.person,
+//   },
+//   {
+//     title: "Notifications",
+//     icon: icons.bell,
+//   },
+//   {
+//     title: "Security",
+//     icon: icons.shield,
+//   },
+//   {
+//     title: "Language",
+//     icon: icons.language,
+//   },
+//   {
+//     title: "Help Center",
+//     icon: icons.info,
+//   },
+//   {
+//     title: "Invite Friends",
+//     icon: icons.people,
+//   },
+// ];
+
+
