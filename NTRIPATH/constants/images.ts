024b183ecdc232dbd3c64a5714c9e23e119eba7d
F1D022143_M.DZAKI AL-Q. <115340796+muhammaddzaki123@users.noreply.github.com>
2025-05-27 02:38:16 +0800

    Add NTRIPATH/constants/images.ts 1748284695691

---
+++ NTRIPATH/constants/images.ts
@@ -0,0 +1,21 @@
+import artikel from "@/assets/images/artikel.png";
+import artikel1 from "@/assets/images/artikel1.png";
+import asupan from "@/assets/images/asupan.png";
+import bg1 from "@/assets/images/bg1.png";
+import diet from "@/assets/images/diet.png";
+import kalkulator from "@/assets/images/kalkulator.png";
+import konsultasi from "@/assets/images/konsultasi.png";
+import logoawal from "@/assets/images/logoawal.png";
+import whiteGradient from "@/assets/images/white-gradient.png";
+
+export default {
+  whiteGradient,
+  logoawal,
+  bg1,
+  artikel,
+  asupan,
+  konsultasi,
+  kalkulator,
+  diet,
+  artikel1,
+};
