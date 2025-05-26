b813708a22532db954649a2c2ded55a5bf7024a5
F1D022143_M.DZAKI AL-Q. <115340796+muhammaddzaki123@users.noreply.github.com>
2025-05-27 02:38:18 +0800

    Add NTRIPATH/constants/icons.ts 1748284697523

---
+++ NTRIPATH/constants/icons.ts
@@ -0,0 +1,29 @@
+import bell from "@/assets/icons/bell.png";
+import edit from "@/assets/icons/edit.png";
+import fillter from "@/assets/icons/filter.png";
+import google from "@/assets/icons/google.png";
+import heart from "@/assets/icons/heart.png";
+import home from "@/assets/icons/home.png";
+import logout from "@/assets/icons/logout.png";
+import page1 from "@/assets/icons/Page1.png";
+import person from "@/assets/icons/person.png";
+import profile from "@/assets/icons/profile.png";
+import rightArrow from "@/assets/icons/right-arrow.png";
+import search from "@/assets/icons/search.png";
+import star from "@/assets/icons/star.png";
+
+export default {
+  google,
+  home,
+  person,
+  search,
+  rightArrow,
+  logout,
+  edit,
+  bell,
+  fillter,
+  star,
+  heart,
+  page1,
+  profile,
+};
